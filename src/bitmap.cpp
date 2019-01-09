5effad0d256d1e273eccb650460422777a6ceb0b
Garrett Folks <folksgl@dukes.jmu.edu>
2019-01-09 18:42:58 -0500

    framework for uci compatibility in place with some of the gui to engine commands now implemented

--- src/bitmap.cpp
+++ src/bitmap.cpp
@@ -1,5 +1,12 @@
 #include "./bitmap.h"
 
+// 64 bit number to be used in the bitmap representation of the board.
+typedef unsigned long long U64;
+
+// Macro to append appropriate suffix to unsigned 64-bit constants
+#define C64(x) x##ULL
+#define U64FULL C64(0xFFFFFFFFFFFFFFFF)
+
 double myfunc(double mynum) {
     return -1;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,72 @@
 #include <iostream>
+#include <cstdlib>
+#include <pthread.h>
 #include "./bitmap.h"
 
 using namespace std;
 
+//#define NUM_THREADS 1
+//#define UCI_THREAD 0
+
+// RESERVED THREADS
+//
+// 0 = stdin/stdout UCI communication thread
+//
+
 int main()
 {
-    cout << "Hello, World!";
+    string uci_token;
+
+    // Get command from stdin and process>
+    do {
+        getline(cin, uci_token);
+
+        if (uci_token == "uci") {
+            cout << "id name Challenger" << endl;
+            cout << "id author folksgl" << endl;
+            cout << "uciok" << endl;
+        }
+        else if (uci_token == "debug") {
+            cout << "You entered debug" << endl;
+        }
+        else if (uci_token == "isready") {
+            cout << "readyok" << endl;
+        }
+        else if (uci_token.substr(0,8) == "setoption") {
+            //TODO
+            cout << "Haven't implemented that yet" << endl;
+        }
+        else if (uci_token.substr(0,7) == "register") {
+            //TODO
+            cout << "Haven't implemented that yet" << endl;
+        }
+        else if (uci_token == "ucinewgame") {
+            //TODO
+            cout << "Haven't implemented that yet" << endl;
+        }
+        else if (uci_token.substr(0,7) == "position") {
+            //TODO
+            cout << "Haven't implemented that yet" << endl;
+        }
+        else if (uci_token.substr(0,1) == "go") {
+            //TODO
+            cout << "Haven't implemented that yet" << endl;
+        }
+        else if (uci_token == "stop") {
+            //TODO
+            cout << "Haven't implemented that yet" << endl;
+        }
+        else if (uci_token == "ponderhit") {
+            //TODO
+            cout << "Haven't implemented that yet" << endl;
+        }
+        else if (uci_token == "quit") {
+            exit(0)
+        }
+
+        // Default action is to do nothing and ignore invalid commands.
+
+    } while (1);
+
     return 0;
 }
