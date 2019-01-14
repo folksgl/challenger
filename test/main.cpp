53e15fda505351f0abb09916d6891485392d2203
Garrett <folksgl@dukes.jmu.edu>
2019-01-14 15:39:30 -0500

    modified to be independent of challenger src/main

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,17 @@
 #include "gtest/gtest.h"
 
+#include "../src/position.h"
+#include "../src/game_variables.h"
+
 using namespace std;
+
+/////////////////////////////////////////////////////////////////////////
+// Include declaration of globals from src/main.cpp since it won't be  //
+// included in compilation of tests.                                   //
+bool G_debug;                                                          // 
+position G_game_position;                                              //
+                                                                       //
+/////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
