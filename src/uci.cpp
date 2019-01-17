e63bfa4a6ccfa93eb7ac7280d060e6a588744812
Garrett <folksgl@dukes.jmu.edu>
2019-01-17 11:27:56 -0500

    refactored -- eliminated multiple calls to substr and use of char*'s

--- src/uci.cpp
+++ src/uci.cpp
@@ -13,39 +13,42 @@ using namespace std;
 void process_uci_commands() {
 
     string uci_token; // The current UCI command read from stdin.
+    string uci_signal;
 
     // Get command from stdin and process>
     while (getline(cin, uci_token)) 
     {
+        uci_signal = uci_token.substr(0, uci_token.find_first_of(" "));
+
         // Output name and author, followed by "uciok".
-        if (uci_token == "uci") {
+        if (uci_signal == "uci") {
             process_uci_signal();
         }
         // Turns on/off debugging.
-        else if (uci_token.substr(0,6) == "debug") {
+        else if (uci_signal == "debug") {
             process_debug_signal(uci_token);
         }
         // Signals the engine is ready to proceed with the next command.
-        else if (uci_token == "isready") {
+        else if (uci_signal == "isready") {
             cout << "readyok" << endl;
         }
-        else if (uci_token.substr(0,9) == "setoption") {
+        else if (uci_signal == "setoption") {
             process_setoption_signal(uci_token);
         }
         // Set up the position given.
-        else if (uci_token.substr(0,8) == "position") {
+        else if (uci_signal == "position") {
             process_position_signal(uci_token);
         }
-        else if (uci_token.substr(0,2) == "go") {
+        else if (uci_signal == "go") {
             process_go_signal(uci_token);
         }
-        else if (uci_token == "stop") {
+        else if (uci_signal == "stop") {
             process_stop_signal();
         }
-        else if (uci_token == "ponderhit") {
+        else if (uci_signal == "ponderhit") {
             process_ponderhit_signal();
         }
-        else if (uci_token == "quit") {
+        else if (uci_signal == "quit") {
             exit(0);
         }
         // Default action is to do nothing and ignore invalid commands.
