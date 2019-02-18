bc5e9595ae8a63eabda461614004e355c8538eeb
Garrett Folks <folksgl@dukes.jmu.edu>
2019-02-17 22:49:07 -0500

    removed common method and squares array to common.cpp/h files

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1,4 +1,5 @@
 #include "evaluate.h"
+#include "common.h"
 
 using namespace std;
 
@@ -61,20 +62,5 @@ int get_black_material_value(Position* pos) {
     return material_val;
 
 
-}
-
-/*
- *  num_set_bits takes in a bitboard and returns the number of bits in that bitboard that are set to 1.
- */
-int num_set_bits(bitboard map) {
-    int num_bits = 0;
-
-    for (int i = 0; i < 64; i++) {
-        if (map & squares[i]) {
-            num_bits++;
-        }
-    }
-
-    return num_bits;
 }
 
