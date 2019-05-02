78b01714ad663acb170c240cd61b3bf2e531bcef
Garrett <folksgl@dukes.jmu.edu>
2019-05-02 11:19:42 -0400

    removed begin methods and added move generation to search process

--- src/search.cpp
+++ src/search.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 /*
- * Perform a search of the position given and return a string representing the best move to make.
+ * Perform a search of the position given.
  */
 void search(Position* pos) {
     if (pos->active_color == 'w') {
@@ -21,50 +21,23 @@ void search(Position* pos) {
     return;
 }
 
-void begin_alpha_search(Position* pos) {
-    int beta  = std::numeric_limits<int>::max();
-    int alpha = std::numeric_limits<int>::min();
-
-    int score = 0;
-
-    for (vector<Position>::iterator p = pos->moves.begin(); p != pos->moves.end(); p++) {
-        Move move;
-        move.value = alphaBetaMax(&(*p), alpha, beta, 15);
-        move.movestring = p->movestring;
-
-        G_movestore->add_move(move);
-
-        if (score <= alpha) {
-            //return alpha;
-        }
-        if (score < beta) {
-            beta = score;
-        }
-    }
-}
-
-void begin_beta_search(Position* pos) {
-    //int beta  = std::numeric_limits<int>::max();
-    //int alpha = std::numeric_limits<int>::min();
-}
-
 int alphaBetaMax(Position* pos, int alpha, int beta, int depth) {
     if (depth == 0) {
         return pos->evaluation_score;
     }
 
+    pos.generate_moves();
+
     std::sort(pos->moves.begin(), pos->moves.end(), 
                       [](Position const &a, Position const &b) { return b.evaluation_score < a.evaluation_score; });
 
-    int score = 0;
-
     for (vector<Position>::iterator p = pos->moves.begin(); p != pos->moves.end(); p++) {
-        score = alphaBetaMin(&(*p), alpha, beta, depth - 1);
-        if (score >= beta) {
+        p->evaluation_score = alphaBetaMin(&(*p), alpha, beta, depth - 1);
+        if (p->evaluation_score >= beta) {
             return beta;
         }
-        if (score > alpha) {
-            alpha = score;
+        if (p->evaluation_score > alpha) {
+            alpha = p->evaluation_score;
         }
     }
     return alpha;
@@ -75,18 +48,18 @@ int alphaBetaMin(Position* pos, int alpha, int beta, int depth) {
         return pos->evaluation_score;
     }
 
+    pos.generate_moves();
+
     std::sort(pos->moves.begin(), pos->moves.end(), 
                       [](Position const &a, Position const &b) { return a.evaluation_score < b.evaluation_score; });
 
-    int score = 0;
-
     for (vector<Position>::iterator p = pos->moves.begin(); p != pos->moves.end(); p++) {
-        score = alphaBetaMax(&(*p), alpha, beta, depth - 1);
-        if (score <= alpha) {
+        p->evaluation_score = alphaBetaMax(&(*p), alpha, beta, depth - 1);
+        if (p->evaluation_score <= alpha) {
             return alpha;
         }
-        if (score < beta) {
-            beta = score;
+        if (p->evaluation_score < beta) {
+            beta = p->evaluation_score;
         }
     }
     return beta;
--- src/search.h
+++ src/search.h
@@ -6,9 +6,6 @@
 
 void search(Position* pos);
 
-void begin_alpha_search(Position* pos);
-void begin_beta_search(Position* pos);
-
 int alphaBetaMax(Position* pos, int alpha, int beta, int depth);
 int alphaBetaMin(Position* pos, int alpha, int beta, int depth);
 
