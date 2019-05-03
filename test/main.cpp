4b6e857d44952a35c4236cc3cba47ce441a68566
Garrett <folksgl@dukes.jmu.edu>
2019-05-03 10:32:57 -0400

    added placeholder tests for uci command functions

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,4 @@
 #include "gtest/gtest.h"
-
 #include "../src/position.h"
 #include "../src/game_variables.h"
 
--- test/ucitest.cpp
+++ test/ucitest.cpp
@@ -1,8 +1,68 @@
 #include "gtest/gtest.h"
+#include "../src/uci.h"
+#include "../src/game_variables.h"
 
 using namespace std;
 
-//TEST (myfuncTest, ZeroAndNegativeNos) { 
-    //ASSERT_EQ (0.0, 0.0);
-//}
+TEST (process_command, uci) { 
+    testing::internal::CaptureStdout();
+    process_uci_command();
+    string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_EQ (output, "id name Challenger\nid author folksgl\nuciok\n");
+}
+
+TEST (process_command, debug_on) { 
+    string debug = "debug on";
+    process_debug_command(debug);
+
+    ASSERT_TRUE(G_debug);
+}
+
+TEST (process_command, debug_off) { 
+    string debug = "debug off";
+    process_debug_command(debug);
+
+    ASSERT_FALSE(G_debug);
+}
+
+TEST (process_command, setoption) { 
+    testing::internal::CaptureStdout();
+    process_uci_command();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_EQ (output, "id name Challenger\nid author folksgl\nuciok\n");
+}
+
+TEST (process_command, position) { 
+    testing::internal::CaptureStdout();
+    process_uci_command();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_EQ (output, "id name Challenger\nid author folksgl\nuciok\n");
+}
+
+TEST (process_command, go) { 
+    testing::internal::CaptureStdout();
+    process_uci_command();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_EQ (output, "id name Challenger\nid author folksgl\nuciok\n");
+}
+
+TEST (process_command, stop) { 
+    testing::internal::CaptureStdout();
+    process_uci_command();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_EQ (output, "id name Challenger\nid author folksgl\nuciok\n");
+}
+
+TEST (process_command, ponderhit) { 
+    testing::internal::CaptureStdout();
+    process_uci_command();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_EQ (output, "id name Challenger\nid author folksgl\nuciok\n");
+}
 
