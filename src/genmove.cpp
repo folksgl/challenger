5a586b2d16eb61e306893ddd3bf736e465394911
Garrett <folksgl@dukes.jmu.edu>
2019-05-08 15:15:22 -0400

    added bitscan optimizations to pawn move generation

--- src/genmove.cpp
+++ src/genmove.cpp
@@ -41,73 +41,76 @@ void generate_black_moves(Position* pos) {
 }
 
 void generate_w_pawn_moves(Position* pos) {
-
-    bitboard passant_bit;
-    if (pos->passant_target_sq == "-" || pos->passant_target_sq.empty()) {
-        passant_bit = 0;
-    }
-    else {
-        passant_bit = squares[get_square_num(pos->passant_target_sq)];
-    }
-
     bitboard pawns = pos->maps[w_pawn];
-    bitboard black = pos->maps[b_pieces] | passant_bit;
+    // check if there are any pawns to generate moves for
+    if (pawns) {
+        bitboard passant_bit = 0;
+        if (pos->passant_target_sq != "-") {
+            passant_bit = squares[get_square_num(pos->passant_target_sq)];
+        }
 
-    // left/right/forward contain the start squares of pawns that can perform those moves.
-    bitboard left_attacks  = (((pawns & (~a_file)) << 7) & black) >> 7;
-    bitboard right_attacks = (((pawns & (~h_file)) << 9) & black) >> 9;
-    bitboard forward = ((pawns << 8) & (~black)) >> 8;
-    bitboard double_forward = ((((pawns & rank_2) & forward) << 16) & (~black)) >> 16;
+        bitboard black = pos->maps[b_pieces] | passant_bit;
 
-    string src, dest;
+        // left/right/forward contain the start squares of pawns that can perform those moves.
+        bitboard left_attacks  = (((pawns & (~a_file)) << 7) & black) >> 7;
+        bitboard right_attacks = (((pawns & (~h_file)) << 9) & black) >> 9;
+        bitboard forward = ((pawns << 8) & (~black)) >> 8;
+        bitboard double_forward = ((((pawns & rank_2) & forward) << 16) & (~black)) >> 16;
 
-    // Loop over pawns and generate appropriate moves.
-    for (int i = 0; i < 64; i++) {
-        // Found a pawn; check for 4 possible moves
-        if (squares[i] & pawns) {
-            src = bit_to_square.at(squares[i]);
+        string src, dest;
 
-            if (squares[i] & left_attacks) {
+        // Loop over pawns and generate appropriate moves.
+        int index = __builtin_ffsll(pawns) - 1;
+        while (index != -1) {
+
+            bitboard squarei = squares[index];
+            src = bit_to_square.at(squarei);
+
+            if (squarei & left_attacks) {
                 Position pawnmove(*pos);
-                dest = bit_to_square.at(squares[i+7]);
+                dest = bit_to_square.at(squares[index+7]);
                 if (dest.at(1) == '8') { dest += 'q'; }
                 pawnmove.movestring = src + dest;
                 pawnmove.move(src+dest);
                 pawnmove.evaluate();
                 pos->moves.push_back(pawnmove);
             }
-            if (squares[i] & right_attacks) {
+            if (squarei & right_attacks) {
                 Position pawnmove(*pos);
-                dest = bit_to_square.at(squares[i+9]);
+                dest = bit_to_square.at(squares[index+9]);
                 if (dest.at(1) == '8') { dest += 'q'; }
                 pawnmove.movestring = src + dest;
                 pawnmove.move(src+dest);
                 pawnmove.evaluate();
                 pos->moves.push_back(pawnmove);
             }
-            if (squares[i] & forward) {
+            if (squarei & forward) {
                 Position pawnmove(*pos);
-                dest = bit_to_square.at(squares[i+8]);
+                dest = bit_to_square.at(squares[index+8]);
                 if (dest.at(1) == '8') { dest += 'q'; }
                 pawnmove.movestring = src + dest;
                 pawnmove.move(src+dest);
                 pawnmove.evaluate();
                 pos->moves.push_back(pawnmove);
             }
-            if (squares[i] & double_forward) {
+            if (squarei & double_forward) {
                 Position pawnmove(*pos);
-                dest = bit_to_square.at(squares[i+16]);
+                dest = bit_to_square.at(squares[index+16]);
                 pawnmove.movestring = src + dest;
                 pawnmove.move(src+dest);
                 pawnmove.evaluate();
                 pos->moves.push_back(pawnmove);
             }
+
+            pawns = pawns & (~squarei);
+            index = __builtin_ffsll(pawns) - 1;
         }
     }
     return;
 }
 
 void generate_w_knight_moves(Position* pos) {
+
     return;
 }
 
@@ -130,67 +133,69 @@ void generate_w_king_moves(Position* pos) {
 
 
 void generate_b_pawn_moves(Position* pos) {
-
-    bitboard passant_bit;
-    if (pos->passant_target_sq == "-" || pos->passant_target_sq.empty()) {
-        passant_bit = 0;
-    }
-    else {
-        passant_bit = squares[get_square_num(pos->passant_target_sq)];
-    }
-
     bitboard pawns = pos->maps[b_pawn];
-    bitboard white = pos->maps[w_pieces] | passant_bit;
+    // check if there are any pawns to generate moves for
+    if (pawns) {
+        bitboard passant_bit = 0;
+        if (pos->passant_target_sq != "-") {
+            passant_bit = squares[get_square_num(pos->passant_target_sq)];
+        }
 
-    // left/right/forward contain the start squares of pawns that can perform those moves.
-    bitboard left_attacks  = (((pawns & (~h_file)) >> 7) & white) << 7;
-    bitboard right_attacks = (((pawns & (~a_file)) >> 9) & white) << 9;
-    bitboard forward = ((pawns >> 8) & (~white)) << 8;
-    bitboard double_forward = ((((pawns & rank_7) & forward) >> 16) & (~white)) << 16;
+        bitboard white = pos->maps[w_pieces] | passant_bit;
 
-    string src, dest;
+        // left/right/forward contain the start squares of pawns that can perform those moves.
+        bitboard left_attacks  = (((pawns & (~h_file)) >> 7) & white) << 7;
+        bitboard right_attacks = (((pawns & (~a_file)) >> 9) & white) << 9;
+        bitboard forward = ((pawns >> 8) & (~white)) << 8;
+        bitboard double_forward = ((((pawns & rank_7) & forward) >> 16) & (~white)) << 16;
 
-    // Loop over pawns and generate appropriate moves.
-    for (int i = 0; i < 64; i++) {
-        // Found a pawn; check for 4 possible moves
-        if (squares[i] & pawns) {
-            src = bit_to_square.at(squares[i]);
+        string src, dest;
 
-            if (squares[i] & left_attacks) {
+        // Loop over pawns and generate appropriate moves.
+        int index = __builtin_ffsll(pawns) - 1;
+        while (index != -1) {
+
+            bitboard squarei = squares[index];
+            src = bit_to_square.at(squarei);
+
+            if (squarei & left_attacks) {
                 Position pawnmove(*pos);
-                dest = bit_to_square.at(squares[i-7]);
+                dest = bit_to_square.at(squares[index-7]);
                 if (dest.at(1) == '8') { dest += 'q'; }
                 pawnmove.movestring = src + dest;
                 pawnmove.move(src+dest);
                 pawnmove.evaluate();
                 pos->moves.push_back(pawnmove);
             }
-            if (squares[i] & right_attacks) {
+            if (squarei & right_attacks) {
                 Position pawnmove(*pos);
-                dest = bit_to_square.at(squares[i-9]);
+                dest = bit_to_square.at(squares[index-9]);
                 if (dest.at(1) == '1') { dest += 'q'; }
                 pawnmove.movestring = src + dest;
                 pawnmove.move(src+dest);
                 pawnmove.evaluate();
                 pos->moves.push_back(pawnmove);
             }
-            if (squares[i] & forward) {
+            if (squarei & forward) {
                 Position pawnmove(*pos);
-                dest = bit_to_square.at(squares[i-8]);
+                dest = bit_to_square.at(squares[index-8]);
                 if (dest.at(1) == '1') { dest += 'q'; }
                 pawnmove.movestring = src + dest;
                 pawnmove.move(src+dest);
                 pawnmove.evaluate();
                 pos->moves.push_back(pawnmove);
             }
-            if (squares[i] & double_forward) {
+            if (squarei & double_forward) {
                 Position pawnmove(*pos);
-                dest = bit_to_square.at(squares[i-16]);
+                dest = bit_to_square.at(squares[index-16]);
                 pawnmove.movestring = src + dest;
                 pawnmove.move(src+dest);
                 pawnmove.evaluate();
                 pos->moves.push_back(pawnmove);
             }
+
+            pawns = pawns & (~squarei);
+            index = __builtin_ffsll(pawns) - 1;
         }
     }
     return;
