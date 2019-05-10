8b591e8ac5df2f728d993c4eec0dc5f00b409cae
folksgl <folksgl@dukes.jmu.edu>
2019-05-10 15:29:53 -0400

    simplified sort logic to improve readability

--- src/search.cpp
+++ src/search.cpp
@@ -6,6 +6,14 @@
 
 using namespace std;
 
+bool desc_sort (Position const &a, Position const &b) {
+    return b.evaluation_score < a.evaluation_score;
+}
+
+bool asc_sort (Position const &a, Position const &b) {
+    return b.evaluation_score > a.evaluation_score;
+}
+
 /*
  * Perform a search of the position given.
  */
@@ -18,14 +26,12 @@ void search(Position* pos, int depth) {
     int beta  = std::numeric_limits<int>::max();
     if (pos->active_color == 'w') {
         alphaBetaMax(pos, alpha, beta, depth);
-        std::sort(pos->moves.begin(), pos->moves.end(), 
-                [](Position const &a, Position const &b) { return b.evaluation_score < a.evaluation_score; });
+        std::sort(pos->moves.begin(), pos->moves.end(), desc_sort);
         
     }
     else {
         alphaBetaMin(pos, alpha, beta, depth);
-        std::sort(pos->moves.begin(), pos->moves.end(), 
-                          [](Position const &a, Position const &b) { return a.evaluation_score < b.evaluation_score; });
+        std::sort(pos->moves.begin(), pos->moves.end(), asc_sort);
     }
 
     return;
@@ -39,8 +45,7 @@ int alphaBetaMax(Position* pos, int alpha, int beta, int depth) {
     pos->generate_moves();
 
     // Sort in descending order of evaluation scores
-    std::sort(pos->moves.begin(), pos->moves.end(), 
-                      [](Position const &a, Position const &b) { return b.evaluation_score < a.evaluation_score; });
+    std::sort(pos->moves.begin(), pos->moves.end(), desc_sort);
 
     for (auto p : pos->moves) {
         p.evaluation_score = alphaBetaMin(&p, alpha, beta, depth - 1);
@@ -62,8 +67,7 @@ int alphaBetaMin(Position* pos, int alpha, int beta, int depth) {
     pos->generate_moves();
 
     // Sort in ascending order of evaluation scores
-    std::sort(pos->moves.begin(), pos->moves.end(), 
-                      [](Position const &a, Position const &b) { return a.evaluation_score < b.evaluation_score; });
+    std::sort(pos->moves.begin(), pos->moves.end(), asc_sort);
 
     for (auto p : pos->moves) {
         p.evaluation_score = alphaBetaMax(&p, alpha, beta, depth - 1);
