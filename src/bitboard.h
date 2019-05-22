99ab8f5b49a16d5af349af79b6098e1744a1c46f
folksgl <folksgl@dukes.jmu.edu>
2019-05-22 13:54:41 -0400

    removed unused macro definitions

--- src/bitboard.h
+++ src/bitboard.h
@@ -6,9 +6,4 @@
 // 64 bit number to be used in the bitboard representation of the board.
 typedef uint64_t bitboard;
 
-// Macro to append appropriate suffix to unsigned 64-bit constants
-#define C64(x) x##ULL
-#define U64FULL C64(0xFFFFFFFFFFFFFFFF)
-
-
 #endif
