80b8c027973451fa3f9788f2eba9d4b1dc945739
folksgl <folksgl@dukes.jmu.edu>
2019-05-27 12:19:53 -0400

    changed bit_to_square from map to array to eliminate hash times

--- src/common.h
+++ src/common.h
@@ -34,6 +34,29 @@ const bitboard rank_6 = 0x0000FF0000000000;
 const bitboard rank_7 = 0x00FF000000000000;
 const bitboard rank_8 = 0xFF00000000000000;
 
+const std::array<std::string, 64> bit_to_square_arr = {
+      "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", 
+      "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", 
+      "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", 
+      "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", 
+      "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", 
+      "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6", 
+      "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", 
+      "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
+};
+
+const std::array<std::string, 64> bit_to_square_pawn_arr = {
+      "a1q", "b1q", "c1q", "d1q", "e1q", "f1q", "g1q", "h1q", 
+      "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", 
+      "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", 
+      "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", 
+      "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", 
+      "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6", 
+      "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", 
+      "a8q", "b8q", "c8q", "d8q", "e8q", "f8q", "g8q", "h8q"
+};
+
+
 const std::unordered_map<bitboard, std::string> bit_to_square({
       {0x00, "-"},
       {0x01, "a1"}, {0x02, "b1"}, 
