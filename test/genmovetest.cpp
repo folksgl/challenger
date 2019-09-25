ccdaa62b39d7102199d2389304cfc51661d42cfc
Garrett <Gfolks@easydynamics.com>
2019-09-25 15:32:34 -0400

    added complex position tests from chessprogramming.org

--- test/genmovetest.cpp
+++ test/genmovetest.cpp
@@ -862,3 +862,48 @@ TEST(correct_moves_generated, middleboard_black_king) {
     EXPECT_EQ(find_move_taken(&startpos, &startpos.moves[7]),   "e5f6");
 }
 
+// Complex positions found on https://www.chessprogramming.org/Perft_Results
+TEST(number_of_moves_generated, complex_position_2) { 
+    std::string test = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+    Position startpos(test);
+
+    generate_moves(&startpos);
+
+    EXPECT_EQ (startpos.moves.size(), 48);
+}
+
+TEST(number_of_moves_generated, complex_position_3) {
+    std::string test = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+    Position startpos(test);
+
+    generate_moves(&startpos);
+
+    EXPECT_EQ (startpos.moves.size(), 14);
+}
+
+TEST(number_of_moves_generated, complex_position_4) {
+    std::string test = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
+    Position startpos(test);
+
+    generate_moves(&startpos);
+
+    EXPECT_EQ (startpos.moves.size(), 6);
+}
+
+TEST(number_of_moves_generated, complex_position_5) {
+    std::string test = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+    Position startpos(test);
+
+    generate_moves(&startpos);
+
+    EXPECT_EQ (startpos.moves.size(), 44);
+}
+
+TEST(number_of_moves_generated, complex_position_6) {
+    std::string test = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
+    Position startpos(test);
+
+    generate_moves(&startpos);
+
+    EXPECT_EQ (startpos.moves.size(), 46);
+}
