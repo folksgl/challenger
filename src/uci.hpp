f77b699357dfb9d6e1ad57a811d65b37b143821d
folksgl <Gfolks14@gmail.com>
2020-11-23 12:47:33 -0500

    remove deleted function headers

--- src/uci.hpp
+++ src/uci.hpp
@@ -13,20 +13,4 @@ void read_commands(std::istream& is);
 
 void process_commands();
 
-void process_uci_command();
-
-void process_debug_command(command_iter begin, command_iter end);
-
-void process_setoption_command(command_iter begin, command_iter end);
-
-void process_position_command(command_iter begin, command_iter end);
-
-void process_go_command(command_iter begin, command_iter end);
-
-void process_stop_command();
-
-void process_ponderhit_command();
-
-bool is_go_subcommand(std::string command);
-
 #endif
